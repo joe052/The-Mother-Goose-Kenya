c4e772ec4fd0bc1600c419676dc4946f4c8a7039
Immanuel Kimani <immanuel4082@gmail.com>
2023-09-22 01:07:24 +0300

    message

--- app/src/components/navbar/navbarIcon.vue
+++ app/src/components/navbar/navbarIcon.vue
@@ -1,5 +1,6 @@
 <template>
   <a class="flex pl-4 items-center font-medium text-gray-900 title-font">
+    <img src="../../assets/images/icon.png" alt="icon" class="h-20">
     <h1 class="text-sm text-blue-700">
       THE <br />MOTHER-GOOSE<br />
       KENYA
