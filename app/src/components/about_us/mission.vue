6f0971356f52e3f990c1ce98247008b95c336af0
Immanuel Kimani <immanuel4082@gmail.com>
2023-09-25 18:06:08 +0300

    message

---
+++ app/src/components/about_us/mission.vue
@@ -0,0 +1,39 @@
+<template>
+  <div
+    class="relative flex items-center justify-center m-5 max-lg:flex-col lg:m-40"
+  >
+    <div class="z-10 lg:translate-x-32">
+      <card
+        ><div class="bg-cyan-700 lg:p-20 p-3 md:p-16">
+          <h1 class="text-2xl font-semibold px-4 text-white">Our Mission</h1>
+          <span class="font-extrabold text-orange-500 p-4"
+            >______________________</span
+          >
+          <p class="text-xl mt-5 text-white">
+            We are guided by the Chapter Four of Kenya’s Constitution, The Bill
+            of Rights, Part 3 section 53 on the rights of children and the
+            founding principles of UNICEF’s Convention on the Rights of the
+            Child (CRC). Our mission is to improve the quality of childcare in
+            communities. We believe that children can only attain their full
+            rights as enshrined in our constitution and UNICEF’s CRC when they
+            live in an environment that accords them the best care.
+          </p>
+        </div>
+      </card>
+    </div>
+    <div
+      class="lg:z-0 bg-cyan-700 overlay lg:-translate-x-36 lg:translate-y-24 md:-translate-y-48"
+    >
+      <img
+        src="./../../assets/images/image1.jpg"
+        alt=""
+        class="h-auto lg:w-[5000px]"
+      />
+      <div
+        class="absolute inset-0 justify-center bg-gradient-to-l from-cyan-700"
+      >
+        <div class="gradient-overlay"></div>
+      </div>
+    </div>
+  </div>
+</template>
--- app/src/views/AboutView.vue
+++ app/src/views/AboutView.vue
@@ -1,9 +1,11 @@
 <template>
   <Header />
+  <Mission />
 </template>
 
 <script setup>
-import Header from '../components/about_us/header.vue';
+import Header from "../components/about_us/header.vue";
+import Mission from "../components/about_us/mission.vue";
 </script>
 
 <style lang="scss" scoped></style>
